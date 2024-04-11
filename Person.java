d0e2168e72af88bad32cd7746ecf97f7bec5737b
andyzheng679 <andyzheng679@gmail.com>
2024-04-11 14:21:00 -0400

    Testing

---
+++ Person.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Person {
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+}
