47802f20ea090296281a7b1cc20ea0bf3a0d5512
andyzheng679 <andyzheng679@gmail.com>
2024-04-12 10:31:03 -0400

    local class

--- Person.java
+++ Person.java
@@ -1,12 +1,94 @@
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Person {
     public enum Sex {
         MALE, FEMALE
     }
 
-    String name;
-    LocalDate birthday;
-    Sex gender;
-    String emailAddress;
+    private String name;
+    private LocalDate birthday;
+    private Sex gender;
+    private String emailAddress;
+    private List<Person> roster = new ArrayList<>();
+
+    public Person(String name, LocalDate birthday, Sex gender, String emailAddress){
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+        this.roster.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public List<Person> getRoster() {
+        return roster;
+    }
+
+    public void setRoster(List<Person> roster) {
+        this.roster = roster;
+    }
+
+    public int getAge(){
+        LocalDate today = LocalDate.now();
+
+        return Period.between(birthday, today).getYears();
+    }
+
+    //local class
+    class CheckPersonEligibleForSelectiveService implements CheckPerson {
+        @Override
+        public boolean test(Person p) {
+            return p.gender == Person.Sex.MALE &&
+                    p.getAge() >= 18 &&
+                    p.getAge() <= 25;
+        }
+    }
+
+    public void printPerson() {
+        System.out.println(name + " (" + gender + ") at " + emailAddress);
+    }
+
+    public static void printPersons(
+            List<Person> roster, CheckPerson tester) {
+        for (Person p : roster) {
+            if (tester.test(p)) {
+                p.printPerson();
+            }
+        }
+    }
+
 }
